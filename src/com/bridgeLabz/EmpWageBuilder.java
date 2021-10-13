3e94267222615d63cdf52a79bb777c1d1005a6e2
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-13 21:30:55 +0530

    Added Daily Wage to Employee wage program

--- src/com/bridgeLabz/EmpWageBuilder.java
+++ src/com/bridgeLabz/EmpWageBuilder.java
@@ -2,15 +2,21 @@ package com.bridgeLabz;
 import java.util.Random;
 public class EmpWageBuilder {
     public static final int IS_PRESENT=1;
+    public static final int IS_FULLTIME=8;
+    public static final int WAGE_PER_HOUR=20;
+    public static int DAILY_WAGE=0;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation program");
         Random random=new Random();
         int Emp_check = random.nextInt(2);
         if (Emp_check == IS_PRESENT){
             System.out.println("Employee is present");
+            DAILY_WAGE=IS_FULLTIME*WAGE_PER_HOUR;
+
         }
         else
             System.out.println("Employee is Absent");
+        System.out.println("Daily wage : " +DAILY_WAGE);
     }
 
 }
