2a2b04761c67f7f758869ed8826497d26dfd42ad
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-13 21:09:59 +0530

    Added Whethet the Employee is present or Absent

--- src/com/bridgeLabz/EmpWageBuilder.java
+++ src/com/bridgeLabz/EmpWageBuilder.java
@@ -1,8 +1,16 @@
 package com.bridgeLabz;
-
+import java.util.Random;
 public class EmpWageBuilder {
+    public static final int IS_PRESENT=1;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation program");
+        Random random=new Random();
+        int Emp_check = random.nextInt(2);
+        if (Emp_check == IS_PRESENT){
+            System.out.println("Employee is present");
+        }
+        else
+            System.out.println("Employee is Absent");
     }
 
 }
